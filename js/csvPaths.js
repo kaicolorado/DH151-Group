77c021d0ff9db5eb7ea60ed943f21c2cc9a58786
Alex Haddad <alexthegreath@gmail.com>
2021-04-28 13:05:29 -0700

    Use Google Sheets CSV path URLs instead of CSV files

---
+++ js/csvPaths.js
@@ -0,0 +1,12 @@
+let csvPath_EducationalSpendingInPublicSchools =
+	"https://docs.google.com/spreadsheets/d/1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18/export?format=csv&id=1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18&gid=0";
+let csvPath_ArtsEducationPolicies =
+	"https://docs.google.com/spreadsheets/d/1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18/export?format=csv&id=1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18&gid=1397950742";
+let csvPath_SP_G4_Math_2019 =
+	"https://docs.google.com/spreadsheets/d/1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18/export?format=csv&id=1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18&gid=1999542441";
+let csvPath_SP_G8_Math_2019 =
+	"https://docs.google.com/spreadsheets/d/1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18/export?format=csv&id=1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18&gid=456758856";
+let csvPath_SP_G4_Reading_2019 =
+	"https://docs.google.com/spreadsheets/d/1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18/export?format=csv&id=1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18&gid=1273736783";
+let csvPath_SP_G8_Reading_2019 =
+	"https://docs.google.com/spreadsheets/d/1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18/export?format=csv&id=1BkXJjCqXiWi_ysFu2Jcnqbo0otuC-BwLhfqJNl1Fx18&gid=960745170";
