fe8286d8d95b589744306761521fd7593c6a45cb
Alex Haddad <alexthegreath@gmail.com>
2021-04-27 20:06:13 -0700

    Add state polygons to map
    added 3 JSON files which are FeatureCollections of state polygon shapes.
    - 500k: most detailed/largest
    - 20m: least detailed/smallest
    - 5m: in between

--- js/map.js
+++ js/map.js
@@ -1,13 +1,15 @@
 let map;
 let markers = L.featureGroup();
 
+let statesJSON;
+
 // initialize
 $(function () {
 	createMap();
 });
 
 // create the map
-function createMap() {
+async function createMap() {
 	map = L.map("map").setView([39.8283, -98.5795], 5); // coords for center of the US
 
 	L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -15,5 +17,11 @@ function createMap() {
 			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	}).addTo(map);
 
-	map.fitBounds(markers.getBounds());
+	// map.fitBounds(markers.getBounds());
+
+	await setUpGeoJSON().then(() => L.geoJson(statesJSON).addTo(map)); // waits until this line finishes before continuing function
+}
+
+async function setUpGeoJSON() {
+	await $.get("../data/states-polygons-20m.json").then((json) => (statesJSON = json));
 }
