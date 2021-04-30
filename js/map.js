a268101670c1fba9afc96b2fcea991e4709e0900
Alex Haddad <alexthegreath@gmail.com>
2021-04-29 22:51:39 -0700

    Implement 10 art policy layers with proper coloring
    - Each art policy layer has a unique color (colors choices WIP) and only
    the states with 'Yes' get colored.
    
    - Moved createLayers call from getting called after getStatePolygons to
    getting called once all the CSV files have been parsed. This is because
    creating these layers depends on the csv file data.

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
-let map;
-var controls = L.control.layers();
+var map;
+const controls = L.control.layers();
 
-var artsEducationPolicyLayers = [];
+const artsEducationPolicyLayers = [];
 var spG4Math2019Layer;
 var spG8Math2019Layer;
 var spG4Reading2019Layer;
@@ -9,7 +9,7 @@ var spG8Reading2019Layer;
 
 var statesJSON;
 
-var csvPaths = [
+const csvPaths = [
 	csvPath_EducationalSpendingInPublicSchools,
 	csvPath_ArtsEducationPolicies,
 	csvPath_SP_G4_Math_2019,
@@ -18,19 +18,19 @@ var csvPaths = [
 	csvPath_SP_G8_Reading_2019,
 ];
 
-var csvData = [];
+const csvData = new Array(csvPaths.length);
 
 // REVIEW: we can either modify the colors of a single layer based on what layers the user selects,
 //         or overlay multiple layers, one for each feature.
 
-// initialize
+//* initialize
 $(function () {
 	createMap();
 	getStatePolygons();
 	readCSVs();
 });
 
-// create the map
+//* create the map
 function createMap() {
 	map = L.map("map").setView([39.8283, -98.5795], 5); // coords for center of the US
 
@@ -43,15 +43,18 @@ function createMap() {
 }
 
 function getStatePolygons() {
-	$.get("../data/states-polygons-20m.json")
-		.then((json) => (statesJSON = json))
-		.then(() => createLayers());
+	$.get("../data/states-polygons-20m.json").then((json) => (statesJSON = json));
 }
 
 function createLayers() {
-	for (var i = 0; i < 10; i++) {
-		artsEducationPolicyLayers.push(L.geoJson(statesJSON));
-		controls.addOverlay(artsEducationPolicyLayers[i], `Arts Education Policy ${i + 1}`);
+	for (var i = 1; i < 11; i++) {
+		const index = i;
+		artsEducationPolicyLayers.push(
+			L.geoJson(statesJSON, {
+				style: (feature) => getArtsEducationPolicyStyle(feature, index),
+			})
+		);
+		controls.addOverlay(artsEducationPolicyLayers[i - 1], `Arts Education Policy ${i}`);
 	}
 
 	// TODO: these 4 layers are currently empty.
@@ -70,13 +73,41 @@ function createLayers() {
 		spG8Reading2019Layer,
 		"Standardized Performances - Grade 8 - Reading - 2019"
 	);
+}
 
-	// var myStyle = {
-	// 	color: "#ff7800",
-	// 	weight: 5,
-	// 	opacity: 0.65,
-	// };
-	// artPoliciesLayer.setStyle(myStyle);
+function getArtsEducationPolicyStyle(feature, index) {
+	//* get the state object who's name (e.g. 'Alabama') matches the state name we're mapping now
+	const state = csvData[1].data.find((row) => row.State === feature.properties.NAME);
+
+	//* if we found a state from above and the current indexed column's result for that state is 'Yes', we want to give it a color
+	if (state && state[Object.keys(state)[index]] === "Yes") {
+		return {
+			fillColor: getArtsEducationPolicyColor(index),
+			color: "black",
+			weight: 0.3,
+		};
+	} else {
+		return {
+			fillOpacity: 0,
+			weight: 0,
+		};
+	}
+}
+
+//* get color based on what art policy we specify (by index)
+function getArtsEducationPolicyColor(index) {
+	// prettier-ignore
+	const fillColor = index == 1 ? "#007AFF" :
+					  index == 2 ? "#FF0000" :
+					  index == 3 ? "#FFE600" :
+					  index == 4 ? "#24FF00" :
+					  index == 5 ? "#00FFA3" :
+					  index == 6 ? "#DB00FF" :
+					  index == 7 ? "#4200FF" :
+					  index == 8 ? "#FF7A00" :
+					  index == 9 ? "#5E382C" :
+					 			   "#6F4200";
+	return fillColor;
 }
 
 async function readCSVs() {
@@ -85,7 +116,14 @@ async function readCSVs() {
 			header: true,
 			download: true,
 			complete: function (data) {
-				csvData.push(data);
+				//* inserts data at a specified index so its index matches the index at csvPath
+				csvData[index] = data;
+
+				//* if the csvData array has no empty elements (meaning all csvPaths have been processed)
+				if (!csvData.includes(undefined)) {
+					//* run this function once all csv files have been loaded
+					createLayers();
+				}
 			},
 		});
 	});
